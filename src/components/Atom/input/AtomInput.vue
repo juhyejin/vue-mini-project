fe18d081dec1e4529b0ea8b207a626137c33017f
juhyejin <juhyejin.dev@gmail.com>
2023-06-01 19:33:20 +0900

    :art: composition API로 변경

--- src/components/Atom/input/AtomInput.vue
+++ src/components/Atom/input/AtomInput.vue
@@ -2,18 +2,15 @@
   <input :type="inputType" :value="inputValue" />
 </template>
 
-<script>
-export default {
-  name: "AtomInput",
-  props:{
-    inputType: {
-      type: String,
-      default: "text",
-    },
-    inputValue: {
-      type: String,
-      default: "",
-    },
+<script setup lang="ts">
+defineProps({
+  inputType:{
+    type: String,
+    default: "text",
   },
-};
+  inputValue: {
+    type: String,
+    default: "",
+  },
+});
 </script>
